349d5b3862b0072fae39bfc52ce0e5b7dc99498b
libang <libang@xiaomi.com>
2016-05-03 15:10:47 +0800

    util funciton add

---
+++ app/src/main/java/com/miapp/util/ImeUtil.java
@@ -0,0 +1,46 @@
+package com.miapp.util;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Created by lebang on 16/5/2.
+ */
+public class ImeUtil {
+    /*
+    * 显示和隐藏软键盘
+    * view : EditText/TextView
+    * isShow : true / false
+    * */
+
+    public static void popSoftKeyboard(Context context, View view, boolean isShow){
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (isShow){
+            view.requestFocus();
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+        }else{
+            imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+        }
+    }
+
+    /*
+    * 显示软键盘
+    * */
+    public static void showSoftKeyboard(View view){
+        Context context = view.getContext();
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        view.requestFocus();
+        imm.showSoftInput(view, InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    /*
+    * 隐藏软键盘
+    * */
+    public static void hideSoftKeyboard(View view){
+        Context context = view.getContext();
+        InputMethodManager imm = (InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+    }
+
+}
