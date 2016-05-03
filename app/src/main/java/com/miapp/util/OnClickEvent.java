6e5b55daf36b137b040c0238ada6ddd27722e199
libang <libang@xiaomi.com>
2016-05-03 10:17:11 +0800

    util funciton add

---
+++ app/src/main/java/com/miapp/util/OnClickEvent.java
@@ -0,0 +1,40 @@
+package com.miapp.util;
+
+import android.view.View;
+
+/**
+ * Created by lebang on 16/5/2.
+ */
+public abstract class OnClickEvent implements View.OnClickListener {
+
+    public static long lastTime;
+    public abstract void singleClick(View v);
+
+    @Override
+    public void onClick(View v) {
+        if(onDoubleClick()){
+            return;
+        }
+        singleClick(v);
+    }
+
+    public boolean onDoubleClick(){
+        boolean flag = false;
+        long time = System.currentTimeMillis() - lastTime;
+
+        if (time < 500){
+            flag = true;
+        }
+        lastTime = System.currentTimeMillis();
+        return  flag;
+    }
+}
+
+/*
+* mClickView.setOnClickListener(new OnClickEvent() {
+    @Override
+    public void singleClick(View v) {
+
+    }
+  });
+* */
